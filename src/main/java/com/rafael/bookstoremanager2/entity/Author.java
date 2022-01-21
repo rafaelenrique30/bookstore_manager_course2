6e7376bc78e9fb1bac473fc9bd99ec4d24becde2
Rafael <rafaelenrique30@gmail.com>
2022-01-20 22:37:05 -0300

    Developed project entities

---
+++ src/main/java/com/rafael/bookstoremanager2/entity/Author.java
@@ -0,0 +1,28 @@
+package com.rafael.bookstoremanager2.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+
+}
