759f3d5ea29935443dcfba2e5e48ffbe85a666d7
Rafael <rafaelenrique30@gmail.com>
2022-01-20 22:10:37 -0300

    First commit: project structure created

---
+++ src/main/java/com/rafael/bookstoremanager2/Bookstoremanager2Application.java
@@ -0,0 +1,13 @@
+package com.rafael.bookstoremanager2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bookstoremanager2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bookstoremanager2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafael/bookstoremanager2/Bookstoremanager2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.bookstoremanager2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bookstoremanager2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
